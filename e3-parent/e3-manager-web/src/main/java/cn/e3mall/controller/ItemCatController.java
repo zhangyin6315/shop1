d3c0b618b9d2475a907517d61c7c93a4327291ae
zhangyin <17852006315@163.com>
2019-09-27 20:22:58 +0800

    新增商品，选择商品类目节点实现

---
+++ e3-parent/e3-manager-web/src/main/java/cn/e3mall/controller/ItemCatController.java
@@ -0,0 +1,29 @@
+package cn.e3mall.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemCatService;
+import cn.e3mall.service.ItemService;
+import cn.e3malll.common.pojo.EasyUIDataFGridResult;
+import cn.e3malll.common.pojo.EasyUITreeNode;
+@Controller
+public class ItemCatController {
+	@Autowired
+	private ItemCatService itemCatService;
+	
+	@RequestMapping("/item/cat/list")
+	@ResponseBody
+	public List<EasyUITreeNode> getItemCatList(@RequestParam(name="id",defaultValue="0") Long parentId) {
+		//调用服务层查询节点列表
+	List<EasyUITreeNode> list=itemCatService.getItemCatlist(parentId);
+	return list;
+	}
+}
