ddb3a25b5611283c848d2458a075c62e9fb862ef
zhangyin <17852006315@163.com>
2019-10-01 18:32:29 +0800

    测试方法

---
+++ e3-parent/e3-manager/e3-manager-service/src/test/java/cn/e3mall/publish/TestPublish.java
@@ -0,0 +1,20 @@
+package cn.e3mall.publish;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestPublish {
+//	@Test
+//public void publicService() throws InterruptedException, IOException {
+//	ApplicationContext applicationContext=new ClassPathXmlApplicationContext("classpath:Spring/applicationContext-*.xml");
+//	while(true) {
+//		Thread.sleep(1000);
+//	}
+//	System.out.println("启动");
+//	System.in.read();
+//	System.out.println("关闭");
+//	}
+}
