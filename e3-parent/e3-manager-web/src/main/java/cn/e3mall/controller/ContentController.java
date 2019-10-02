0945362b8c58976be00c69d87ad452e091b81bc4
zhangyin <17852006315@163.com>
2019-10-02 22:14:14 +0800

    引入redis缓存

--- e3-parent/e3-manager-web/src/main/java/cn/e3mall/controller/ContentController.java
+++ e3-parent/e3-manager-web/src/main/java/cn/e3mall/controller/ContentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -24,13 +25,29 @@ public class ContentController {
 	@RequestMapping(value="/content/save",method=RequestMethod.POST)
 	@ResponseBody
 	public E3Result addContent(TbContent content){
-		E3Result erResult=contentService.addcontent(content);
-		return erResult;
+		E3Result e3Result=contentService.addcontent(content);
+		return e3Result;
 	}
 	@RequestMapping(value="/content/query/list")
 	@ResponseBody
 	public List<TbContent> getContentList( Long categoryId){
 		List<TbContent> list=contentService.getContentList(categoryId);
 		return list;
+	}
+//	"/rest/content/edit",$("#contentEditForm").serialize(), function(data)
+	@RequestMapping(value="/rest/content/edit",method=RequestMethod.POST)
+	@ResponseBody
+	public E3Result editContent(TbContent content) {
+		E3Result e3Result=contentService.editContent(content);
+		return e3Result;
+		
+	}
+//	/content/delete
+	@RequestMapping(value="/content/delete",method=RequestMethod.POST)
+	@ResponseBody
+	public E3Result deleteContent(@RequestParam("ids") String ids) {
+		E3Result e3Result=contentService.deleteContent(ids);
+		return e3Result;
+		
 	}
 }
