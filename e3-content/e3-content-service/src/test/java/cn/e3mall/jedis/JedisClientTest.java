1b08415f03f6fd45951fcfed9f86c3a98207da10
zhangyin <17852006315@163.com>
2019-10-06 09:30:44 +0800

    其他文件

--- e3-content/e3-content-service/src/test/java/cn/e3mall/jedis/JedisClientTest.java
+++ e3-content/e3-content-service/src/test/java/cn/e3mall/jedis/JedisClientTest.java
@@ -1,18 +1,19 @@
-//package cn.e3mall.jedis;
-//
-//import org.junit.Test;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-//
-//import cn.e3mall.common.jedis.JedisClient;
-//
-//public class JedisClientTest {
-//	@Test
-//public void testJedisClientTest() {
-//	//初始化Spring 容器
-//		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("classpath:spring/applicationContext-redis.xml");
-//		JedisClient jedisClient=applicationContext.getBean(JedisClient.class);
-////		jedisClient.set("345", "value");
-//		System.out.println(jedisClient.get("345"));
-//}
-//}
+package cn.e3mall.jedis;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.e3mall.common.jedis.JedisClient;
+
+public class JedisClientTest {
+	@Test
+public void testJedisClientTest() {
+	//初始化Spring 容器
+		@SuppressWarnings("resource")
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("classpath:spring/applicationContext-redis.xml");
+		JedisClient jedisClient=applicationContext.getBean(JedisClient.class);
+//		jedisClient.set("345", "value");
+		System.out.println(jedisClient.get("345"));
+}
+}
